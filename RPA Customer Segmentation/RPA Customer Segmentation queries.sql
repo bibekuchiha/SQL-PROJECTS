1cff5a013706f9e2b3405208e36fba134596a391
Bibek Shah Shankhar <57903320+bibekuchiha@users.noreply.github.com>
2020-08-11 09:32:19 +0545

    Create RPA Customer Segmentation queries.sql

---
+++ RPA Customer Segmentation/RPA Customer Segmentation queries.sql
@@ -0,0 +1,36 @@
+ --1.Start by getting a feel for the users table:
+
+ SELECT * FROM users LIMIT 20;
+
+--2.Find the email addresses and birthdays of users whose birthday is between 1980-01-01 and 1989-12-31.
+
+ SELECT email , birthday 
+ FROM users
+ WHERE birthday BETWEEN '1980-01-01' AND '1989-12-31';
+
+--3.Find the emails and creation date of users whose created_at date matches this condition.
+
+ SELECT email, created_at
+ FROM users
+ WHERE created_at <= '2017-05-01';
+
+--4. Find the emails of the users who received the ‘bears’ test.
+
+ SELECT email FROM users
+ WHERE test = 'bears';
+
+--5.Find all the emails of all users who received a campaign on website BBB.
+
+ SELECT email FROM users
+ WHERE campaign LIKE 'BBB%';
+
+--6. Find all the emails of all users who received ad copy 2 in their campaign.
+
+ SELECT email FROM users
+ WHERE campaign LIKE '%-2';
+
+--7. Find the emails for all users who received both a campaign and a test. 
+
+ SELECT email FROM users
+ WHERE campaign IS NOT NULL
+ AND test IS NOT NULL;
